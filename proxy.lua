02ea8f0ec4296c063eeaf16946306de7d025c38e
Lawvy <150415573+Lawvy@users.noreply.github.com>
2025-06-07 14:19:04 +0700

    Update proxy.lua

--- proxy.lua
+++ proxy.lua
@@ -34,12 +34,15 @@ add_spacer|small|
 add_button_with_icon|wrenchlink|`@Wrench|staticPurpleFrame|32||
 add_button_with_icon|loglink|`@Logging|staticPurpleFrame|2916||
 add_button_with_icon|spinlink|`@Roulette|staticPurpleFrame|758||
-add_button_with_icon||END_LIST|noflags|0||
 add_button_with_icon|otherlink|`@Other|staticPurpleFrame|3180||
+add_button_with_icon||END_LIST|noflags|0||
 add_button_with_icon|proxy|`@Commands|staticPurpleFrame|12658||
+add_button_with_icon|changelog|`@Changelog|staticPurpleFrame|3524||
+add_button_with_icon|contact|`@Contact|staticPurpleFrame|8028||
 add_button_with_icon|social|`@Social Portal|staticPurpleFrame|1366||
 
-
+add_quick_exit
+end_dialog|menuproxy|Close|
 ]]
 
 function bubble(str)
