9456bb03adc8dd24561714e74479c8e9d490b3e0
Lawvy <150415573+Lawvy@users.noreply.github.com>
2025-06-07 14:23:33 +0700

    Update proxy.lua

--- proxy.lua
+++ proxy.lua
@@ -31,6 +31,12 @@ add_label_with_icon|big|`#Proxy Menu|left|12658|
 
 add_spacer|small|
 
+add_textbox|Hi, ]]..GetLocal().name..[[|
+add_textbox|Your Using Free Beta Version Of Pandora|
+add_smalltext|Found a bug? Contact Me On Discord @lv3not7221|
+
+add_spacer|small|
+
 add_button_with_icon|wrenchlink|`@Wrench|staticPurpleFrame|32||
 add_button_with_icon|loglink|`@Logging|staticPurpleFrame|2916||
 add_button_with_icon|spinlink|`@Roulette|staticPurpleFrame|758||
@@ -40,6 +46,7 @@ add_button_with_icon|proxy|`@Commands|staticPurpleFrame|12658||
 add_button_with_icon|changelog|`@Changelog|staticPurpleFrame|3524||
 add_button_with_icon|contact|`@Contact|staticPurpleFrame|8028||
 add_button_with_icon|social|`@Social Portal|staticPurpleFrame|1366||
+add_button_with_icon||END_LIST|noflags|0||
 
 add_quick_exit
 end_dialog|menuproxy|Close|
